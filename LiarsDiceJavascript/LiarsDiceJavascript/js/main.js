f82304522dc20696a483de9c34a21a569bbace6b
Emil Christiansen <34371682+Emsaso@users.noreply.github.com>
2018-06-15 09:21:50 +0200

    Add files via upload

---
+++ LiarsDiceJavascript/LiarsDiceJavascript/js/main.js
@@ -0,0 +1,22 @@
+﻿var playerOneName = "Emil";
+var playerTwoName = "Hellenes";
+var playerThreeName = "Lill June";
+var playerFourName = "Kvamme";
+var playerFiveName = "Terje";
+var playerSixName = "Eskil";
+var playerSevenName = "Geir";
+var playerEightName = "Eddie";
+
+
+
+var newElement = document.createElement("div");
+newElement.setAttribute("class", "player" + i);
+newElement.innerHTML = "Player" + i;
+var div = document.createElement("div");
+div.innerHTML = "Emil";
+
+function StartGame() {
+    for (var i = 0; i < 8; i++) {
+        document.getElementsByClassName("player")[i].innerHTML = playerOneName;
+    }
+}
