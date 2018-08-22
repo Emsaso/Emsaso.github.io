72f6b9b01e580f56f650cc6904ec27781077ac7d
Emil Christiansen <34371682+Emsaso@users.noreply.github.com>
2018-08-22 10:50:24 +0200

    Add files via upload

---
+++ AreProject/AreProject.js
@@ -0,0 +1,13 @@
+<script src="https://www.gstatic.com/firebasejs/5.4.0/firebase.js"></script>
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDGR_t2OmqYx4kV43uhLSDBSxQudi8SWzw",
+    authDomain: "sunlight-scripture.firebaseapp.com",
+    databaseURL: "https://sunlight-scripture.firebaseio.com",
+    projectId: "sunlight-scripture",
+    storageBucket: "sunlight-scripture.appspot.com",
+    messagingSenderId: "1065419366338"
+};
+firebase.initializeApp(config);
+
