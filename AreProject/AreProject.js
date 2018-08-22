9f7d82c195b2dea5d667fadc71da21102c37acb4
Emil Christiansen <34371682+Emsaso@users.noreply.github.com>
2018-08-22 15:09:25 +0200

    2

--- AreProject/AreProject.js
+++ AreProject/AreProject.js
@@ -1,4 +1,3 @@
-
 // Initialize Firebase
 var config = {
     apiKey: "AIzaSyDGR_t2OmqYx4kV43uhLSDBSxQudi8SWzw",
@@ -10,13 +9,83 @@ var config = {
 };
 firebase.initializeApp(config);
 var db = firebase.firestore();
-var firebase = firebase.firestore();
 const settings = { timestampsInSnapshots: true };
 db.settings(settings);
 var databaseId;
 
+var database = firebase.database();
+function initFirebase(test, divamount) {
+    db.collection("main").doc("test").set({
+        divamount: 31
+    });
+}
 
-var apekatt = document.createElement(`MonkeyCat`);
-apekatt.innerHTML = "Apekatt";
-document.getElementById("mainDiv").appendChild(apekatt);
+var bodyStyle = document.body.style;
 
+var mainDiv = document.createElement(`div`);
+mainDiv.innerHTML = "Hello";
+mainDiv.id = "mainDiv";
+mainDiv.style.margin = "1vh 1vw";
+mainDiv.style.background = "grey";
+mainDiv.style.borderRadius = "1em";
+mainDiv.style.textAlign = "center";
+document.body.appendChild(mainDiv);
+
+var secondDiv = document.createElement(`div`);
+secondDiv.innerHTML = "Hello";
+secondDiv.id = "secondDiv";
+secondDiv.style.background = "red";
+secondDiv.style.textAlign = "center";
+document.getElementById("mainDiv").appendChild(secondDiv);
+
+var thirdDiv = document.createElement(`div`);
+thirdDiv.innerHTML = "Hello";
+thirdDiv.id = "thirdDiv";
+thirdDiv.style.background = "blue";
+thirdDiv.style.borderRadius = "1em";
+thirdDiv.style.textAlign = "center";
+document.getElementById("secondDiv").appendChild(thirdDiv);
+
+var fourthDiv = document.createElement(`div`);
+fourthDiv.innerHTML = "Hello";
+fourthDiv.id = "fourthDiv";
+fourthDiv.style.background = "green";
+fourthDiv.style.borderRadius = "1em";
+fourthDiv.style.textAlign = "center";
+document.getElementById("thirdDiv").appendChild(fourthDiv);
+
+var fifthDiv = document.createElement(`div`);
+fifthDiv.innerHTML = "Hello";
+fifthDiv.id = "fifthDiv";
+fifthDiv.style.background = "yellow";
+fifthDiv.style.borderRadius = "1em";
+fifthDiv.style.textAlign = "center";
+document.getElementById("fourthDiv").appendChild(fifthDiv);
+
+var sixthDiv = document.createElement(`div`);
+sixthDiv.innerHTML = "Hello";
+sixthDiv.id = "sixthDiv";
+sixthDiv.style.background = "purple";
+sixthDiv.style.borderRadius = "1em";
+sixthDiv.style.textAlign = "center";
+document.getElementById("fifthDiv").appendChild(sixthDiv);
+
+var seventhDiv = document.createElement(`div`);
+seventhDiv.innerHTML = "Hello";
+seventhDiv.id = "seventhDiv";
+seventhDiv.style.background = "pink";
+seventhDiv.style.borderRadius = "1em";
+seventhDiv.style.textAlign = "center";
+document.getElementById("sixthDiv").appendChild(seventhDiv);
+
+for (var i = 0; i <= 100/*divamount*/; i++) {
+    var eighthDiv = document.createElement(`div`);
+    eighthDiv.innerHTML = `Hello ${i / 100}`;
+    eighthDiv.id = "eighthDiv";
+    eighthDiv.style.background = "black";
+    eighthDiv.style.borderRadius = "1em";
+    eighthDiv.style.textAlign = "center";
+    eighthDiv.style.color = "white";
+    eighthDiv.style.fontSize = "4.9px";
+    document.getElementById("seventhDiv").appendChild(eighthDiv);
+}
