d4838e78c164f487dbafe584f92e79ab07f64db5
Emil Christiansen <34371682+Emsaso@users.noreply.github.com>
2018-08-29 15:30:25 +0200

    dfghjkl

---
+++ MonkeyFactsDotEmil/MonkeyFactsDotEmil/bin/Debug/AppX/js/main.js
@@ -0,0 +1,12 @@
+﻿
+function myFunction() {
+    var i = 1;
+    var x = document.getElementById(`picture${i}`);
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        i++;
+    } else {
+        x.style.display = "none";
+    }
+console.log(i);
+}
---
+++ MonkeyFactsDotEmil/MonkeyFactsDotEmil/js/main.js
@@ -0,0 +1,17 @@
+﻿var i = 1;
+
+function myFunction() {
+    var x = document.getElementById(`picture${i}`);
+    var m = document.getElementById(`div${y}`);
+    x.style.display = "none";
+    i++;
+    if (i > 10) {
+        for (let y = 10; y > 0; y--) {
+            var z = document.getElementById(`picture${y}`);
+            var m = document.getElementById(`div${y}`);
+            z.style.display = "block";
+            m.innerHTML = "block";
+        }
+        console.log(x, m, i);
+    }
+}
