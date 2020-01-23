ad2222a74bb1541da10d59e97d1f78bb305f7c57
Emsaso <34371682+Emsaso@users.noreply.github.com>
2020-01-23 13:35:24 +0100

    Update enkelLøpeUnitTesting.js

--- JoggingWithCoordinates/enkelLøpeUnitTesting.js
+++ JoggingWithCoordinates/enkelLøpeUnitTesting.js
@@ -11,6 +11,6 @@ QUnit.test("current position", function (assert) {
     let seconds = 30;
     let estimatedPosition = getLatLonTest(trailData, raceData, seconds);
     let guessedPosition = [50.0000025, 50.0000025];
-    assert.equal(estimatedPosition[0], 50.0000025, `Estimated latitude (${estimatedPosition[0]}) is the same as guessed latitude (${guessedPosition[0]})`);
-    assert.equal(estimatedPosition[1], 50.0000025, `Estimated longitude (${estimatedPosition[1]}) is the same as guessed longitude (${guessedPosition[1]})`);
+    assert.equal(estimatedPosition[0], 50.0000025, `lat: ${estimatedPosition[0]} = ${guessedPosition[0]}`);
+    assert.equal(estimatedPosition[1], 50.0000025, `lon: ${estimatedPosition[1]} = ${guessedPosition[1]}`);
 });
