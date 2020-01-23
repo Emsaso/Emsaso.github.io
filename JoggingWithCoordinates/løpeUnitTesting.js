2c93820e2a8de181f32977fb153c2ab41d28602d
Emsaso <34371682+Emsaso@users.noreply.github.com>
2020-01-23 10:52:03 +0100

    x

--- JoggingWithCoordinates/løpeUnitTesting.js
+++ JoggingWithCoordinates/løpeUnitTesting.js
@@ -1,4 +1,24 @@
-QUnit.test("checkpoints", function (assert) {
+QUnit.test("1224 seconds", function (assert) {
     let chosenAmountOfSecondsSinceStartOfRace = 1224;
+    assert.ok(true, getLatLonOnce(chosenAmountOfSecondsSinceStartOfRace));
+});
+QUnit.test("1337 seconds", function (assert) {
+    let chosenAmountOfSecondsSinceStartOfRace = 1337;
+    assert.ok(true, getLatLonOnce(chosenAmountOfSecondsSinceStartOfRace));
+});
+QUnit.test("6969 seconds", function (assert) {
+    let chosenAmountOfSecondsSinceStartOfRace = 6969;
+    assert.ok(true, getLatLonOnce(chosenAmountOfSecondsSinceStartOfRace));
+});
+QUnit.test("2020 seconds", function (assert) {
+    let chosenAmountOfSecondsSinceStartOfRace = 2020;
+    assert.ok(true, getLatLonOnce(chosenAmountOfSecondsSinceStartOfRace));
+});
+QUnit.test("12345 seconds", function (assert) {
+    let chosenAmountOfSecondsSinceStartOfRace = 12345;
+    assert.ok(true, getLatLonOnce(chosenAmountOfSecondsSinceStartOfRace));
+});
+QUnit.test("2468 seconds", function (assert) {
+    let chosenAmountOfSecondsSinceStartOfRace = 2468;
     assert.ok(true, getLatLonOnce(chosenAmountOfSecondsSinceStartOfRace));
 });
