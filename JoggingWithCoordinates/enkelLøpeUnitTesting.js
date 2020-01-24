3e93b4e83479abe06fd6560d98f7f0e2853eba42
Emsaso <34371682+Emsaso@users.noreply.github.com>
2020-01-24 10:29:23 +0100

    Update enkelLøpeUnitTesting.js

--- JoggingWithCoordinates/enkelLøpeUnitTesting.js
+++ JoggingWithCoordinates/enkelLøpeUnitTesting.js
@@ -27,14 +27,42 @@
         },
     ];
 
-
-    QUnit.test("current position", function (assert) {
-        let seconds = 30;
+    function estimateAndGuess(seconds) {
         let estimatedPosition = getLatLonTest(trailData, raceData, seconds);
         let guessedPosition = [
             trailData[0].lat + ((raceData[1].location.lat - raceData[0].location.lat) * seconds / raceData[1].time),
             trailData[0].lon + ((raceData[1].location.lon - raceData[0].location.lon) * seconds / raceData[1].time)
         ]
+        return [estimatedPosition, guessedPosition]
+    }
+
+    QUnit.test("current position", function (assert) {
+        let seconds = 10;
+        let estimatedPosition = estimateAndGuess(seconds)[0], guessedPosition = estimateAndGuess(seconds)[1];
+        assert.equal(estimatedPosition[0], guessedPosition[0], `lat: ${estimatedPosition[0]} = ${guessedPosition[0]}`);
+        assert.equal(estimatedPosition[1], guessedPosition[1], `lon: ${estimatedPosition[1]} = ${guessedPosition[1]}`);
+        assert.equal(seconds, seconds, `seconds: ${seconds}`);
+    });
+
+    QUnit.test("current position", function (assert) {
+        let seconds = 30;
+        let estimatedPosition = estimateAndGuess(seconds)[0], guessedPosition = estimateAndGuess(seconds)[1];
+        assert.equal(estimatedPosition[0], guessedPosition[0], `lat: ${estimatedPosition[0]} = ${guessedPosition[0]}`);
+        assert.equal(estimatedPosition[1], guessedPosition[1], `lon: ${estimatedPosition[1]} = ${guessedPosition[1]}`);
+        assert.equal(seconds, seconds, `seconds: ${seconds}`);
+    });
+
+    QUnit.test("current position", function (assert) {
+        let seconds = 40;
+        let estimatedPosition = estimateAndGuess(seconds)[0], guessedPosition = estimateAndGuess(seconds)[1];
+        assert.equal(estimatedPosition[0], guessedPosition[0], `lat: ${estimatedPosition[0]} = ${guessedPosition[0]}`);
+        assert.equal(estimatedPosition[1], guessedPosition[1], `lon: ${estimatedPosition[1]} = ${guessedPosition[1]}`);
+        assert.equal(seconds, seconds, `seconds: ${seconds}`);
+    });
+
+    QUnit.test("current position", function (assert) {
+        let seconds = 400;
+        let estimatedPosition = estimateAndGuess(seconds)[0], guessedPosition = estimateAndGuess(seconds)[1];
         assert.equal(estimatedPosition[0], guessedPosition[0], `lat: ${estimatedPosition[0]} = ${guessedPosition[0]}`);
         assert.equal(estimatedPosition[1], guessedPosition[1], `lon: ${estimatedPosition[1]} = ${guessedPosition[1]}`);
         assert.equal(seconds, seconds, `seconds: ${seconds}`);
@@ -42,11 +70,15 @@
 
     QUnit.test("current position", function (assert) {
         let seconds = 500;
-        let estimatedPosition = getLatLonTest(trailData, raceData, seconds);
-        let guessedPosition = [
-            trailData[0].lat + ((raceData[1].location.lat - raceData[0].location.lat) * seconds / raceData[1].time),
-            trailData[0].lon + ((raceData[1].location.lon - raceData[0].location.lon) * seconds / raceData[1].time)
-        ]
+        let estimatedPosition = estimateAndGuess(seconds)[0], guessedPosition = estimateAndGuess(seconds)[1];
+        assert.equal(estimatedPosition[0], guessedPosition[0], `lat: ${estimatedPosition[0]} = ${guessedPosition[0]}`);
+        assert.equal(estimatedPosition[1], guessedPosition[1], `lon: ${estimatedPosition[1]} = ${guessedPosition[1]}`);
+        assert.equal(seconds, seconds, `seconds: ${seconds}`);
+    });
+
+    QUnit.test("current position", function (assert) {
+        let seconds = 4000;
+        let estimatedPosition = estimateAndGuess(seconds)[0], guessedPosition = estimateAndGuess(seconds)[1];
         assert.equal(estimatedPosition[0], guessedPosition[0], `lat: ${estimatedPosition[0]} = ${guessedPosition[0]}`);
         assert.equal(estimatedPosition[1], guessedPosition[1], `lon: ${estimatedPosition[1]} = ${guessedPosition[1]}`);
         assert.equal(seconds, seconds, `seconds: ${seconds}`);
@@ -54,11 +86,15 @@
 
     QUnit.test("current position", function (assert) {
         let seconds = 10000;
-        let estimatedPosition = getLatLonTest(trailData, raceData, seconds);
-        let guessedPosition = [
-            trailData[0].lat + ((raceData[1].location.lat - raceData[0].location.lat) * seconds / raceData[1].time),
-            trailData[0].lon + ((raceData[1].location.lon - raceData[0].location.lon) * seconds / raceData[1].time)
-        ]
+        let estimatedPosition = estimateAndGuess(seconds)[0], guessedPosition = estimateAndGuess(seconds)[1];
+        assert.equal(estimatedPosition[0], guessedPosition[0], `lat: ${estimatedPosition[0]} = ${guessedPosition[0]}`);
+        assert.equal(estimatedPosition[1], guessedPosition[1], `lon: ${estimatedPosition[1]} = ${guessedPosition[1]}`);
+        assert.equal(seconds, seconds, `seconds: ${seconds}`);
+    });
+
+    QUnit.test("current position", function (assert) {
+        let seconds = 40000;
+        let estimatedPosition = estimateAndGuess(seconds)[0], guessedPosition = estimateAndGuess(seconds)[1];
         assert.equal(estimatedPosition[0], guessedPosition[0], `lat: ${estimatedPosition[0]} = ${guessedPosition[0]}`);
         assert.equal(estimatedPosition[1], guessedPosition[1], `lon: ${estimatedPosition[1]} = ${guessedPosition[1]}`);
         assert.equal(seconds, seconds, `seconds: ${seconds}`);
@@ -66,11 +102,7 @@
 
     QUnit.test("current position", function (assert) {
         let seconds = 50000;
-        let estimatedPosition = getLatLonTest(trailData, raceData, seconds);
-        let guessedPosition = [
-            trailData[0].lat + ((raceData[1].location.lat - raceData[0].location.lat) * seconds / raceData[1].time),
-            trailData[0].lon + ((raceData[1].location.lon - raceData[0].location.lon) * seconds / raceData[1].time)
-        ]
+        let estimatedPosition = estimateAndGuess(seconds)[0], guessedPosition = estimateAndGuess(seconds)[1];
         assert.equal(estimatedPosition[0], guessedPosition[0], `lat: ${estimatedPosition[0]} = ${guessedPosition[0]}`);
         assert.equal(estimatedPosition[1], guessedPosition[1], `lon: ${estimatedPosition[1]} = ${guessedPosition[1]}`);
         assert.equal(seconds, seconds, `seconds: ${seconds}`);
@@ -78,11 +110,7 @@
 
     QUnit.test("current position", function (assert) {
         let seconds = 999999;
-        let estimatedPosition = getLatLonTest(trailData, raceData, seconds);
-        let guessedPosition = [
-            trailData[0].lat + ((raceData[1].location.lat - raceData[0].location.lat) * seconds / raceData[1].time),
-            trailData[0].lon + ((raceData[1].location.lon - raceData[0].location.lon) * seconds / raceData[1].time)
-        ]
+        let estimatedPosition = estimateAndGuess(seconds)[0], guessedPosition = estimateAndGuess(seconds)[1];
         assert.equal(estimatedPosition[0], guessedPosition[0], `lat: ${estimatedPosition[0]} = ${guessedPosition[0]}`);
         assert.equal(estimatedPosition[1], guessedPosition[1], `lon: ${estimatedPosition[1]} = ${guessedPosition[1]}`);
         assert.equal(seconds, seconds, `seconds: ${seconds}`);
