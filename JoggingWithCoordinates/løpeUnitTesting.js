4b7c6da6d51dd3aa2d96d6dd4795eb6e774ea6da
Emsaso <34371682+Emsaso@users.noreply.github.com>
2020-01-27 14:49:14 +0100

    Update løpeUnitTesting.js

--- JoggingWithCoordinates/løpeUnitTesting.js
+++ JoggingWithCoordinates/løpeUnitTesting.js
@@ -1,3 +1,9 @@
+// for (let i = 0; i < 100; i++) {
+//     QUnit.test(i + " seconds", function (assert) {
+//         assert.ok(true, getLatLonOnce(i));
+//     });
+// }
+
 QUnit.test("1224 seconds", function (assert) {
     let chosenAmountOfSecondsSinceStartOfRace = 1224;
     assert.ok(true, getLatLonOnce(chosenAmountOfSecondsSinceStartOfRace));
@@ -6,19 +12,27 @@ QUnit.test("1337 seconds", function (assert) {
     let chosenAmountOfSecondsSinceStartOfRace = 1337;
     assert.ok(true, getLatLonOnce(chosenAmountOfSecondsSinceStartOfRace));
 });
-QUnit.test("6969 seconds", function (assert) {
-    let chosenAmountOfSecondsSinceStartOfRace = 6969;
-    assert.ok(true, getLatLonOnce(chosenAmountOfSecondsSinceStartOfRace));
-});
 QUnit.test("2020 seconds", function (assert) {
     let chosenAmountOfSecondsSinceStartOfRace = 2020;
+    assert.ok(true, getLatLonOnce(chosenAmountOfSecondsSinceStartOfRace));
+});
+QUnit.test("2468 seconds", function (assert) {
+    let chosenAmountOfSecondsSinceStartOfRace = 2468;
+    assert.ok(true, getLatLonOnce(chosenAmountOfSecondsSinceStartOfRace));
+});
+QUnit.test("6969 seconds", function (assert) {
+    let chosenAmountOfSecondsSinceStartOfRace = 6969;
     assert.ok(true, getLatLonOnce(chosenAmountOfSecondsSinceStartOfRace));
 });
 QUnit.test("12345 seconds", function (assert) {
     let chosenAmountOfSecondsSinceStartOfRace = 12345;
     assert.ok(true, getLatLonOnce(chosenAmountOfSecondsSinceStartOfRace));
 });
-QUnit.test("2468 seconds", function (assert) {
-    let chosenAmountOfSecondsSinceStartOfRace = 2468;
+QUnit.test("23456 seconds", function (assert) {
+    let chosenAmountOfSecondsSinceStartOfRace = 23456;
+    assert.ok(true, getLatLonOnce(chosenAmountOfSecondsSinceStartOfRace));
+});
+QUnit.test("34567 seconds", function (assert) {
+    let chosenAmountOfSecondsSinceStartOfRace = 34567;
     assert.ok(true, getLatLonOnce(chosenAmountOfSecondsSinceStartOfRace));
 });
