a3c86c50d1af2f1b9de96e6e3be6af7e3e164d14
Emsaso <34371682+Emsaso@users.noreply.github.com>
2020-02-12 10:32:40 +0100

    Update løpeUnitTesting.js

--- JoggingWithCoordinates/løpeUnitTesting.js
+++ JoggingWithCoordinates/løpeUnitTesting.js
@@ -5,17 +5,29 @@ getDocument(selectedDocument);
 
 QUnit.test("1224 seconds", function (assert) {
     let chosenAmountOfSecondsSinceStartOfRace = 1224;
-    assert.equal([59.5625919447851,10.18771916865797], getLatLonOnce(chosenAmountOfSecondsSinceStartOfRace, experience, weather));
+    let glloShort = getLatLonOnce(chosenAmountOfSecondsSinceStartOfRace, experience, weather);
+    let fakeCoordinates = [59.5625919447851,10.18771916865797]; 
+    assert.equal(fakeCoordinates, glloShort, fakeCoordinates == glloShort);
+    assert.deepEqual(fakeCoordinates, glloShort, fakeCoordinates == glloShort);
 });
 QUnit.test("12345 seconds", function (assert) {
     let chosenAmountOfSecondsSinceStartOfRace = 12345;
-    assert.equal([59.46732148875834,10.228051380002926], getLatLonOnce(chosenAmountOfSecondsSinceStartOfRace, experience, weather));
+    let glloShort = getLatLonOnce(chosenAmountOfSecondsSinceStartOfRace, experience, weather);
+    let fakeCoordinates = [59.46732148875834,10.228051380002926]; 
+    assert.equal(fakeCoordinates, glloShort, fakeCoordinates == glloShort);
+    assert.deepEqual(fakeCoordinates, glloShort, fakeCoordinates == glloShort);
 });
 QUnit.test("23456 seconds", function (assert) {
     let chosenAmountOfSecondsSinceStartOfRace = 23456;
-    assert.equal([59.381012898441334,10.309122682239586], getLatLonOnce(chosenAmountOfSecondsSinceStartOfRace, experience, weather));
+    let glloShort = getLatLonOnce(chosenAmountOfSecondsSinceStartOfRace, experience, weather);
+    let fakeCoordinates = [59.381012898441334,10.309122682239586]; 
+    assert.equal(fakeCoordinates, glloShort, fakeCoordinates == glloShort);
+    assert.deepEqual(fakeCoordinates, glloShort, fakeCoordinates == glloShort);
 });
 QUnit.test("34567 seconds", function (assert) {
     let chosenAmountOfSecondsSinceStartOfRace = 34567;
-    assert.equal([59.30597157542984,10.406561770154166], getLatLonOnce(chosenAmountOfSecondsSinceStartOfRace, experience, weather));
+    let glloShort = getLatLonOnce(chosenAmountOfSecondsSinceStartOfRace, experience, weather);
+    let fakeCoordinates = [59.30597157542984,10.406561770154166]; 
+    assert.equal(fakeCoordinates, glloShort, fakeCoordinates == glloShort);
+    assert.deepEqual(fakeCoordinates, glloShort, fakeCoordinates == glloShort);
 });
