bfb86a67d3b4840b06c56d807c49b505f5018d99
Emsaso <34371682+Emsaso@users.noreply.github.com>
2020-02-20 14:35:00 +0100

    Update sjekkHvertSekund.js

--- JoggingWithCoordinates/sjekkHvertSekund.js
+++ JoggingWithCoordinates/sjekkHvertSekund.js
@@ -1,17 +1,13 @@
 window.setInterval(repeatEverySecond, 1000);
-let latitude = document.createElement("div");
-document.body.appendChild(latitude);
-let longitude = document.createElement("div");
-document.body.appendChild(longitude);
-let seconds = document.createElement("div");
-document.body.appendChild(seconds);
-
 let startTime = Date.now();
 
 function repeatEverySecond() {
     let secondsSinceStart = Math.floor((Date.now() - startTime) / 1000);
-    let result = getLatLonOnce(secondsSinceStart, calculateProwess("pro"), calculateRain("clearWeather"));
-    latitude.innerHTML = "Lat: " + result[0];
-    longitude.innerHTML = "Lon: " + result[1];
-    seconds.innerHTML = "Seconds since start: " + secondsSinceStart;
+    let experience = calculateProwess("pro");
+    let weather = calculateRain("clearWeather");
+    let result = getLatLonOnce(secondsSinceStart, experience, weather);
+
+    QUnit.test("seconds", function (assert) {
+        assert.deepEqual(`${result[0]}, ${result[1]}, ${secondsSinceStart}`);
+    });
 }
